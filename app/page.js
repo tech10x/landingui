462b4ac29dd2b9805c1d26b561b76608edffbb49
Kunal Chandra Das <kunalchandradasofficial@gmail.com>
2024-05-24 01:31:13 -0700

    Hero-section-create

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import HomeBanner from '@/components/ordinary/home-banner/HomeBanner.jsx';
 import Marquee from '@/components/ordinary/marquee/Marquee';
+import LeftHero from '@/components/reusable/hero-section/LeftHero';
+import RightHero from '@/components/reusable/hero-section/RightHero';
+import launchFastImg from '@/assets/images/Dedicated-Image/landing-page-hero-sec-img/launch-fast.svg';
+import scaleFasterImg from '@/assets/images/Dedicated-Image/landing-page-hero-sec-img/scale-faster.svg';
+import manageBetterImg from '@/assets/images/Dedicated-Image/landing-page-hero-sec-img/manage-better.svg';
 
-
+import firstHeroBulletIcon from '@/assets/icons/bullet-purple-icon-removebg-preview.png';
+import secondHeroBulletIcon from '@/assets/icons/black-blue-bulltet-point.webp';
+import thridHeroBulletIcon from '@/assets/icons/green-bullet-point.png';
 
 const Home = () => {
   return (
@@ -10,7 +17,34 @@ const Home = () => {
 
       <HomeBanner />
       <Marquee />
+      <div className='block mx-auto text-2xl font-bold w-3/4  mt-28 mb-20 text-center sm:text-4xl'>
+        Whether you’re a startup or an established business, here’s why Tech10x is your best choice
+      </div>
+      <LeftHero referredImg={launchFastImg} title={"Launch Fast"}
+        itemOne={"Fully responsive e-commerce website & mobile app."}
+        itemTwo={"Loads 6X faster than existing solutions."}
+        itemThree={"Upload/import products and inventory in bulk."}
+        itemFour={"Integrate payment gateways."}
+        itemFive={"Easily customizable themes."}
+        bulletPoint={firstHeroBulletIcon} />
 
+
+      <RightHero referredImg={scaleFasterImg} title={"Scale Faster"}
+        itemOne={"Guaranteed 99.5% uptime for your store - We keep you open for business."}
+        itemTwo={"60+ third party plugins."}
+        itemThree={"Marketing tools and discounts to drive repeat orders."}
+        itemFour={"Add staff accounts, assign different roles."}
+        itemFive={"Unlimited transactions, minimal transaction fees."}
+        bulletPoint={secondHeroBulletIcon} />
+
+
+      <LeftHero referredImg={manageBetterImg} title={"Manage Better"}
+        itemOne={"Order tracking, invoicing and order reports."}
+        itemTwo={"Bulk edit product prices, variants, inventory."}
+        itemThree={"Manage global deliveries."}
+        itemFour={"In-depth business analytics."}
+        itemFive={"Automate all tax calculations."}
+        bulletPoint={thridHeroBulletIcon} />
     </>
   )
 }
---
+++ components/reusable/hero-section/RightHero.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Image from 'next/image';
+
+
+
+const RightHero = ({ referredImg, title, itemOne, itemTwo, itemThree, itemFour, itemFive, bulletPoint }) => {
+    return (
+        <div>
+            <div className='px-4 lg:px-24 flex items-center'>
+                <div className='flex w-full flex-col md:flex-row justify-around items-center gap-12 py-8 flex-col-reverse'>
+
+
+                    {/* Left Content  */}
+                    <div className='xl:ml-28 2xl:mx-auto space-y-8 h-full'>
+                        <h4 className='text-3xl font-bold'>{title}</h4>
+                        <ul className='flex flex-col'>
+
+                            <li className='inline-flex items-center my-1.5 text-lg font-semibold'>
+                                <Image src={bulletPoint} height={22} width={22} alt='Bulltet-Points' className='mr-3' />
+                                {itemOne}
+                            </li>
+
+                            <li className='inline-flex items-center my-1.5 text-lg font-semibold'>
+                                <Image src={bulletPoint} height={22} width={22} alt='Bulltet-Points' className='mr-3' />
+                                {itemTwo}
+                            </li>
+
+                            <li className='inline-flex items-center my-1.5 text-lg font-semibold'>
+                                <Image src={bulletPoint} height={22} width={22} alt='Bulltet-Points' className='mr-3' />
+                                {itemThree}
+                            </li>
+
+                            <li className='inline-flex items-center my-1.5 text-lg font-semibold'>
+                                <Image src={bulletPoint} height={22} width={22} alt='Bulltet-Points' className='mr-3' />
+                                {itemFour}
+                            </li>
+
+                            <li className='inline-flex items-center my-1.5 text-lg font-semibold'>
+                                <Image src={bulletPoint} height={22} width={22} alt='Bulltet-Points' className='mr-3' />
+                                {itemFive}
+                            </li>
+
+                        </ul>
+                    </div>
+
+
+                    {/* Right Content  */}
+                    <div className='md:w-2/4 lg:w-1/2 space-y-8 h-full'>
+                        <Image src={referredImg}
+                            alt={title}
+                            width={500}
+                            height={500}
+                            quality={100}
+                            loading="eager"
+                            priority={true} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RightHero;
