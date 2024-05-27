8994fae8594a20f37f6a253decde18d764c8e592
Kunal Chandra Das <kunalchandradasofficial@gmail.com>
2024-05-27 13:08:24 -0700

    Service-section and Card-section-add

--- components/ordinary/services-section/ServicesSec.jsx
+++ components/ordinary/services-section/ServicesSec.jsx
@@ -1,13 +1,64 @@
+import CardSection from '@/components/reusable/card-section/CardSection';
 import React from 'react';
+import SiteSpeedIcon from '@/assets/icons/cards-icon/Site-Speed-Icon.png';
+import MultiWarehouse from '@/assets/icons/cards-icon/Warehouse-icon.png';
+import OptimisedCheckout from '@/assets/icons/cards-icon/Checkout-icon.png';
+import StaffAccounts from '@/assets/icons/cards-icon/Staff-Acount-icon.png';
+import AndroidApp from '@/assets/icons/cards-icon/Android-app-icon.png';
+import AdvancedAnalytics from '@/assets/icons/cards-icon/Advance-Analysis-icon.png';
 
 const ServicesSec = () => {
+    const cardContent = [
+        {
+            imgUrl: SiteSpeedIcon,
+            title: "Site Speed",
+            textContent: "Incredibly fast storefronts. Don't take our word for it, start selling online and see it for yourself!"
+        },
+        {
+            imgUrl: MultiWarehouse,
+            title: "Multi-Warehouse",
+            textContent: "One store, multiple locations. Ship products from multiple warehouses across India."
+        },
+        {
+            imgUrl: OptimisedCheckout,
+            title: "Optimised Checkouts",
+            textContent: "Offer a seamless shopping experience optimised for checkouts and reduce abandonment rates."
+        },
+        {
+            imgUrl: StaffAccounts,
+            title: "Staff Accounts",
+            textContent: "Add employees, colleagues and teammates to help you grow your business while managing access."
+        },
+        {
+            imgUrl: AndroidApp,
+            title: "Android App",
+            textContent: "The world is mobile. It's time your store is too. Get more loyal customers with your mobile app."
+        },
+        {
+            imgUrl: AdvancedAnalytics,
+            title: "Advanced Analytics",
+            textContent: "All the information about your sales, traffic, regions and products, just a single click away."
+        }
+    ];
     return (
-        <section className='h-screen border bg-pink-50'>
+        <section className='bg-pink-50 pb-12 pt-2'>
             <div className='flex flex-col text-center my-16'>
-                <h5 className='text-4xl font-bold'>E-commerce Simplified, Success Amplified</h5>
-                <h6 className='text-[1.3rem] font-medium text-gray-500 mt-4'>Empowering your online business growth with all the essential tools.</h6>
+                <h5 className='text-2xl font-bold lg:text-4xl md:text-3xl'>E-commerce Simplified, Success Amplified</h5>
+                <h6 className='text-[0.9rem] lg:text-[1.3rem] md:text-[1rem] font-medium text-gray-500 mt-4'>Empowering your online business growth with all the essential tools.</h6>
             </div>
-            <div>Cards item</div>
+
+            {/* Card Items  */}
+            <div className="flex flex-wrap justify-center mt-10">
+
+                {
+                    cardContent.map(({ imgUrl, title, textContent }) =>
+                        <CardSection key={title} referImg={imgUrl}
+                            title={title}
+                            content={textContent} />)
+                }
+
+            </div>
+
         </section>
     )
 }
---
+++ components/reusable/card-section/CardSection.jsx
@@ -0,0 +1,46 @@
+'use client';
+import { React, useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Image from 'next/image';
+
+
+const CardSection = ({ referImg, title, content }) => {
+
+
+    useEffect(() => {
+        AOS.init()
+    }, []);
+
+
+    return (
+
+        <div>
+            <div className="p-4 max-w-sm" data-aos="zoom-in" >
+                <div className="flex rounded-lg h-full dark:bg-gray-800 bg-white p-8 flex-col shadow-inner">
+                    <div className="flex flex-col items-center my-3">
+                        <div className='mb-4'>
+                            <Image src={referImg}
+                                alt='Site-Speed-Icon'
+                                quality={70}
+                                loading="lazy"
+                                height={100}
+                                width={100}
+                                className='hover:rounded-md transition-transform hover:scale-110' />
+                        </div>
+                        <h2 className="text-gray-900 dark:text-white text-xl font-medium">{title}</h2>
+                    </div>
+                    <div className="flex flex-col justify-between flex-grow">
+                        <p className="leading-relaxed text-base lg:text-[1.1rem] md:text-[1rem] text-gray-500 dark:text-gray-300">
+                            {content}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default CardSection;
