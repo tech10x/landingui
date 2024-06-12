57fd80d73058d9091c4d7b92c697a15659a7689f
Kunal Ch Das <kunalchandradasofficial@gmail.com>
2024-06-12 14:31:31 +0530

    delete-bad-code

--- utils/form-submit-status/Status.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-export const Status = (para) => {
-    const [uploadStatus, setUploadStatus] = useState(false);
-    if(para === true){
-        setUploadStatus(true);
-    }
-  return uploadStatus;
-}
