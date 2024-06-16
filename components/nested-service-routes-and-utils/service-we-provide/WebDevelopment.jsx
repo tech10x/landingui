ea288dbed451688fb0186688360b9c690e079a10
Kunal Ch Das <kunalchandradasofficial@gmail.com>
2024-06-16 14:34:31 +0530

    animation-add-on-service-section

--- components/nested-service-routes-and-utils/service-we-provide/WebDevelopment.jsx
+++ components/nested-service-routes-and-utils/service-we-provide/WebDevelopment.jsx
@@ -3,13 +3,37 @@ import Step from '@/components/reusable/steps/Step';
 import TechnologyWeUse from '@/components/reusable/technology-we-use/TechnologyWeUse';
 import WhyTech10x from '@/components/reusable/why-tech10x/WhyTech10x';
 import React from 'react';
+import javascript from '@/assets/icons/technology-icon/javascript.png';
+import reactJs from '@/assets/icons/technology-icon/react-js.png';
+import nodeJs from '@/assets/icons/technology-icon/node-js.png';
+import expressJs from '@/assets/icons/technology-icon/express.png';
+import nextJs from '@/assets/icons/technology-icon/next-js.png';
+import typeScript from '@/assets/icons/technology-icon/typescript.png';
+import mongoDb from '@/assets/icons/technology-icon/mongo-db.png';
+import python from '@/assets/icons/technology-icon/python.png';
+import java from '@/assets/icons/technology-icon/java.png';
+import tailwind from '@/assets/icons/technology-icon/tailwind.png';
+import ruby from '@/assets/icons/technology-icon/ruby.png';
+import angular from '@/assets/icons/technology-icon/angular.png';
 
 const WebDevelopment = () => {
   return (
     <div>
       <WhyTech10x />
       <Step />
-      <TechnologyWeUse />
+      <TechnologyWeUse 
+      tech1={javascript}
+      tech2={reactJs} 
+      tech3={nodeJs} 
+      tech4={expressJs} 
+      tech5={nextJs} 
+      tech6={typeScript} 
+      tech7={mongoDb} 
+      tech8={python} 
+      tech9={java} 
+      tech10={tailwind} 
+      tech11={ruby} 
+      tech12={angular}/>
       <DevFeature />
     </div>
   )
--- components/reusable/technology-we-use/TechnologyWeUse.jsx
+++ components/reusable/technology-we-use/TechnologyWeUse.jsx
@@ -2,7 +2,23 @@ import Image from 'next/image'
 import React from 'react';
 import tempLogo from '@/assets/icons/tech10x.webp';
 
-const TechnologyWeUse = () => {
+const TechnologyWeUse = ({tech1, tech2, tech3, tech4, tech5, tech6, tech7, tech8, tech9, tech10, tech11, tech12}) => {
+
+  const items = [
+    tech1,
+    tech2,
+    tech3,
+    tech4,
+    tech5,
+    tech6,
+    tech7,
+    tech8,
+    tech9,
+    tech10,
+    tech11,
+    tech12
+
+  ]
   return (
     <div>
 <section className="text-gray-600 body-font">
@@ -11,93 +27,21 @@ const TechnologyWeUse = () => {
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Technology We Will Be Using</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably heard of them.</p>
     </div>
-    <div className="flex flex-wrap -m-2">
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
-            <p className="text-gray-500">UI Designer</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Henry Letham</h2>
-            <p className="text-gray-500">CTO</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Oskar Blinde</h2>
-            <p className="text-gray-500">Founder</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">John Doe</h2>
-            <p className="text-gray-500">DevOps</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Martin Eden</h2>
-            <p className="text-gray-500">Software Engineer</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Boris Kitua</h2>
-            <p className="text-gray-500">UX Researcher</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Atticus Finch</h2>
-            <p className="text-gray-500">QA Engineer</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Alper Kamu</h2>
-            <p className="text-gray-500">System</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <Image alt="team" className="w-16 h-16 bg-green-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={tempLogo}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
-            <p className="text-gray-500">Product Manager</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 lg:gap-12">
+
+{
+items.map((item) =>
+<div key={item} className="h-32 rounded-lg bg-green-50 flex flex-col justify-center items-center globalImageAndIconAnimation">
+<Image alt="team" className="w-20 h-20 object-cover object-center flex-shrink-0" src={item}/>
+</div> )
+}
+</div>
   </div>
 </section>
     </div>
   )
 }
 
-export default TechnologyWeUse
+export default TechnologyWeUse;
+
+
